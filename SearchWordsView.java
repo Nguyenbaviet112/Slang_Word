133cddd4662dc0d9efae8f740024ff77deb7508b
Nguyễn Bá Việt <51191112+Nguyenbaviet112@users.noreply.github.com>
2022-06-30 08:23:43 +0700

    Update SearchWordsView load data JTable

--- SearchWordsView.java
+++ SearchWordsView.java
@@ -7,16 +7,30 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import controller.SearchWordsListener;
+import model.SlangWordModel;
+
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.Font;
+import java.util.List;
+import java.util.Map;
+
 import javax.swing.JTextField;
 import javax.swing.JList;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
 
 public class SearchWordsView extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField_search;
+	private SlangWordModel slangWordModel;
+	private DefaultTableModel defaultTableModel;
+	private JTable table;
+	
 
 	/**
 	 * Launch the application.
@@ -46,6 +60,13 @@ public class SearchWordsView extends JFrame {
 		contentPane.setLayout(null);
 		setLocationRelativeTo(null);
 		
+		
+		
+		slangWordModel = new SlangWordModel();
+		
+		SearchWordsListener dc = new SearchWordsListener(this);
+		
+		
 		JPanel panel_top = new JPanel();
 		panel_top.setBounds(10, 10, 894, 173);
 		contentPane.add(panel_top);
@@ -64,14 +85,68 @@ public class SearchWordsView extends JFrame {
 		panel_top.add(textField_search);
 		textField_search.setColumns(10);
 		
+		textField_search.getDocument().addDocumentListener(dc);
+		
 		JPanel panel_center = new JPanel();
 		panel_center.setBounds(10, 211, 894, 395);
 		contentPane.add(panel_center);
+		
+		
+		defaultTableModel = new DefaultTableModel(
+				new Object[][] {
+					{null, null},
+				},
+				new String[] {
+					"SlangWord", "Definition"
+				}
+			);
 		panel_center.setLayout(null);
 		
-		JList list = new JList();
-		list.setBounds(10, 27, 860, 358);
-		list.setPreferredSize(new Dimension(200, 200));
-		panel_center.add(list);
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 10, 874, 278);
+		panel_center.add(scrollPane);
+		
+		table = new JTable(defaultTableModel);
+		scrollPane.setViewportView(table);
+		
+		
+	
+		
+	
+		
+
+	}
+	
+	public void showTextField()
+	{
+		System.out.println(textField_search.getText());
+	}
+	
+	public void loadSlangWord()
+	{
+		this.slangWordModel.get_SlangWord_Definition();
+
+		
+		
+		for (Map.Entry<String, List<String>> entry : this.slangWordModel.getWordList().entrySet()) {
+			defaultTableModel.addRow(
+	                new Object[]{
+	                      entry.getKey(), 
+	                      entry.getValue(),                   
+	                });
+		}
+		
+		for (Map.Entry<String, List<String>> entry : this.slangWordModel.getWordList_1().entrySet()) {
+			defaultTableModel.addRow(
+	                new Object[]{
+	                      entry.getKey(), 
+	                      entry.getValue(),                   
+	                });
+		}
+		
+		table.repaint();
+		
+		
+		
 	}
 }
