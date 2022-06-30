7ecd42e70a06921742f69902ad257e3e8e5fc555
Nguyễn Bá Việt <51191112+Nguyenbaviet112@users.noreply.github.com>
2022-06-30 15:43:54 +0700

    Update SearchWordsListener.java

--- SearchWordsListener.java
+++ SearchWordsListener.java
@@ -1,40 +1,28 @@
 package controller;
 
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
 
 import view.SearchWordsView;
 
 
-public class SearchWordsListener implements DocumentListener  {
+public class SearchWordsListener implements ActionListener  {
 
 	private SearchWordsView searchWordsView;
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		String button = e.getActionCommand();
+		
+		if (button.equals("Search"))
+		{
+			this.searchWordsView.loadSlangWord();
+		}
+	}
 	
-	public SearchWordsListener(SearchWordsView searchWordsView)
-	{
-		this.searchWordsView = searchWordsView;
-	}
-
-	@Override
-	public void insertUpdate(DocumentEvent e) {
-		// TODO Auto-generated method stub
-		this.searchWordsView.loadSlangWord();
-	}
-
-	@Override
-	public void removeUpdate(DocumentEvent e) {
-		// TODO Auto-generated method stub
-		this.searchWordsView.loadSlangWord();
-		
-	}
-
-	@Override
-	public void changedUpdate(DocumentEvent e) {
-		this.searchWordsView.loadSlangWord();
-		
-	}
+	
 	
 
 		
