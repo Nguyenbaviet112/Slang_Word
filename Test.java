52bec07a38b020f22a51f0f909b5f8cb6dc016e9
Nguyễn Bá Việt <51191112+Nguyenbaviet112@users.noreply.github.com>
2022-06-30 10:47:13 +0700

    commit

--- Test.java
+++ Test.java
@@ -1,10 +1,10 @@
 package test;
 
 import model.SlangWordModel;
-import view.SearchWordsView;
+import view.MenuView;
 
 public class Test {
 	public static void main(String[] args) {
-		
+		new MenuView();
 	}
 }
